085e5f22ff6dd2f213e5cc3c5df8e28e33bae9a5
zito-s <jhhj424@naver.com>
2022-01-22 12:31:17 +0900

    feat: add shop, styler entity

---
+++ src/main/java/com/example/demo/test/domain/around/operation/styler/Styler.java
@@ -0,0 +1,22 @@
+package com.example.demo.test.domain.around.operation.styler;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Styler {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
